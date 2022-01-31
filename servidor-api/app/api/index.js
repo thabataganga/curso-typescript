865999dea85e805aed05029fa11b4577c0aeaebd
thabataganga <ganga.thabata@gmail.com>
2022-01-31 17:39:00 -0300

    consumindo api externa

--- servidor-api/app/api/index.js
+++ servidor-api/app/api/index.js
@@ -1,7 +1,6 @@
 /* Código simplório, apenas para fornecer o serviço para a aplicação */
 var api = {}
 
-
 api.dados = function(req, res) {
 
     res.json([
@@ -10,7 +9,7 @@ api.dados = function(req, res) {
         { montante: 50.5, vezes: 1 },
         { montante: 70.5, vezes: 2 }
     ]);
-    
+
 };
 
 
